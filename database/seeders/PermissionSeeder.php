8fa4af790999c3b0558ff1ea8e1b25b3aadbb0ae
adarsh-vj <adarsh@kawikatechnologies.com>
2024-05-27 16:21:19 +0530

    Changed permission seeder to check data is already available in the database

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Permission;
 
@@ -16,70 +15,60 @@ class PermissionSeeder extends Seeder
     public function run()
     {
         $permissions = $this->getPermissions();
+
         foreach ($permissions as $val) {
-            Permission::create($val);
+            // Use firstOrCreate to avoid duplicate entries
+            Permission::firstOrCreate(
+                ['name' => $val['name']], // Attributes to search for
+                ['sub_permission' => $val['sub_permission']] // Attributes to set if not found
+            );
         }
     }
 
     public function getPermissions()
     {
         return [
-
             ['name' => 'user-management', 'sub_permission' => json_encode([
                 "users-list",
                 "add-user",
                 "edit-user",
                 "delete-user"
-                ]
-            )],
+            ])],
             ['name' => 'role-management', 'sub_permission' => json_encode([
                 "role-list",
                 "add-role",
                 "edit-role",
                 "delete-role"
-
-                ]
-            )],
+            ])],
             ['name' => 'permission-management', 'sub_permission' => json_encode([
                 "permission-list",
                 "add-permission",
                 "edit-permission"
-                ]
-            )],
+            ])],
             ['name' => 'case-management', 'sub_permission' => json_encode([
                 "case-list",
                 "add-case",
                 "edit-case",
                 "delete-case"
-
-                ]
-            )],
+            ])],
             ['name' => 'police-station-management', 'sub_permission' => json_encode([
                 "police-station-list",
                 "add-police-station",
                 "edit-police-station",
                 "delete-police-station"
-
-                ]
-            )],
+            ])],
             ['name' => 'orders-management', 'sub_permission' => json_encode([
                 "orders-list",
                 "add-orders",
                 "edit-orders",
                 "delete-orders"
-
-                ]
-            )],
+            ])],
             ['name' => 'panchayat-management', 'sub_permission' => json_encode([
                 "panchayat-list",
                 "add-panchayat",
                 "edit-panchayat",
                 "delete-panchayat"
-
-                ]
-            )],
-
-
+            ])],
         ];
     }
 }
